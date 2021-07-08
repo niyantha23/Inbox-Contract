e217bcc78484d988c81bfd5907c363e3d363066d
niyantha23 <niyantha23052001@gmail.com>
2021-07-08 19:16:04 +0530

    tests using Mocha

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,24 +1,43 @@
 const assert = require('assert')
-const ganache =require('ganache-cli')
-const Web3=require('web3')
-const web3=new Web3(ganache.provider())
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+const web3 = new Web3(ganache.provider())
 
-class Car{
+// class Car {
 
-    park(){
-        return 'stopped'
+//     park() {
+//         return 'stopped'
 
-    }
+//     }
 
-    drive(){
-        return 'vroom'
-    }
-}
+//     drive() {
+//         return 'vroom'
+//     }
+// }
+// let car
 
-describe('Car',()=>{
-    it('can park',()=>{
-        const car=new Car()
-        assert.equal(car.park(),'stopped');
+// beforeEach(() => {
+//     car = new Car()
+// })
+// describe('Car', () => {
+//     it('can park', () => {
+//         assert.equal(car.park(), 'stopped');
 
+//     })
+
+//     it('can drive', () => {
+//         assert.equal(car.drive(), 'vroom')
+//     })
+// })
+
+let accounts
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts()
+
+})
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+        console.log(accounts)
     })
 })
