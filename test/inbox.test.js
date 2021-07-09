39582a28b09fb77837d8819980ff13288137f214
niyantha23 <niyantha23052001@gmail.com>
2021-07-09 11:16:50 +0530

    deployed contract

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,6 +2,7 @@ const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
 const web3 = new Web3(ganache.provider())
+const { interface, bytecode } = require('../compile')
 
 // class Car {
 
@@ -31,13 +32,16 @@ const web3 = new Web3(ganache.provider())
 // })
 
 let accounts
-
+let inbox
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts()
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000' })
 
 })
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(accounts)
+        console.log(inbox)
     })
 })
